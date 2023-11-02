75709ae3302b9d449104c46e7a31986637b6d400
JuanesVpcb <je.vasquez2@uniandes.edu.co>
2023-11-02 12:43:59 -0500

    Added support for the login and password in views

--- Entrega2/src/GraphicalInterface/ClientView.java
+++ Entrega2/src/GraphicalInterface/ClientView.java
@@ -4,6 +4,16 @@ import javax.swing.*;
 
 public class ClientView extends JPanel {
 
-    
+    private String login;
+    private String password;
+
+    ClientView(String login, String password) {
+
+        this.login = login;
+        this.password = password;
+
+        // TODO: Construct the Panel with its attributes and specificities
+
+    }
     
 }
